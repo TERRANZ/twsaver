c65f4a56f7b1a56a20c35b332b4f1ee8c61c12dd
Vadim_Korostelev <terranz@terranz.mine.nu>
2016-07-13 17:27:36 +0300

    moved to dms

--- src/main/java/ru/terra/twitsaver/Main.java
+++ src/main/java/ru/terra/twitsaver/Main.java
@@ -15,17 +15,15 @@ import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.apache.log4j.BasicConfigurator;
 import org.codehaus.jackson.map.ObjectMapper;
-import org.slf4j.LoggerFactory;
+import ru.terra.dms.configuration.Configuration;
+import ru.terra.dms.configuration.bean.Pojo;
+import ru.terra.dms.rest.RestService;
+import ru.terra.dms.shared.dto.ObjectDTO;
+import ru.terra.server.dto.LoginDTO;
 import ru.terra.twitsaver.dto.Medium;
 import ru.terra.twitsaver.dto.Twit;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -33,13 +31,24 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
+    public static String USER = "awd";
+    public static String PASS = "awd";
+
     public static void run(String... tag) throws InterruptedException, IOException {
         Client client = null;
         try {
+            LoginDTO ret = RestService.getInstance().login(USER, PASS);
+            if (ret.logged)
+                RestService.getInstance().setSession(ret.session);
+            final RestService restService = RestService.getInstance();
+            final Configuration configuration = restService.loadConfiguration();
+            System.out.println(configuration.toString());
+            final Pojo md5Pojo = configuration.getPojo("TerraFile");
+            System.out.println(md5Pojo.toString());
             ExecutorService service = Executors.newFixedThreadPool(30);
             BlockingQueue<String> queue = new LinkedBlockingQueue<>(10000);
             StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
-            endpoint.followings(Lists.newArrayList(558929794L));
+            endpoint.followings(Lists.newArrayList(558929794L, 2224934952L, 256253131L));
             Authentication auth = new OAuth1("mxkj9PpSNFO7Qgkmpasq1ec4S", "nLtcxVNsnT3lhtSf9qraSlEJ6u4uK9m0jRR0JIHBEBzZnnJTEc", "2550132680-cPN1zH41SJMnSzPEgp7eyouaCXbZYIE6sOCVDIv", "TzCBXwp7elFJZQqIs869EVtB03WskjuULhWLuHC9fRkBG");
             client = new ClientBuilder()
                     .hosts(Constants.STREAM_HOST)
@@ -55,15 +64,16 @@ public class Main {
                     int MESSAGES = 1;
                     if (queue.size() > MESSAGES) {
                         List<String> messages = new ArrayList();
-                        queue.drainTo(messages, 20);
+                        queue.drainTo(messages, queue.size());
                         service.submit(() -> {
                                     for (String msg : messages) {
                                         try {
                                             Twit twit = new ObjectMapper().readValue(msg, Twit.class);
+                                            service.submit(() -> saveTwit(restService, twit));
                                             List<Medium> media = twit.getEntities().getMedia();
                                             if (media.size() > 0) {
-                                                LoggerFactory.getLogger(Main.class).info(msg);
-                                                service.submit(() -> media.forEach(m -> downloadImage("images/" + produceFileName(), m.getMediaUrl())));
+//                                                LoggerFactory.getLogger(Main.class).info(msg);
+                                                service.submit(() -> media.forEach(m -> downloadImage(restService, "images/" + produceFileName(), m.getMediaUrl())));
                                             }
                                             if (twit.getExtendedEntities() != null && twit.getExtendedEntities().getMedia() != null && twit.getExtendedEntities().getMedia().size() > 0) {
                                                 List<Medium> extendedMedia = twit.getExtendedEntities().getMedia();
@@ -73,7 +83,7 @@ public class Main {
                                                     if (variants.size() > 0)
                                                         for (Object v : variants)
                                                             if (((Map) v).containsKey("url"))
-                                                                downloadImage("images/" + produceFileName(), (String) ((Map) v).get("url"));
+                                                                downloadImage(restService, "images/" + produceFileName(), (String) ((Map) v).get("url"));
                                                 }));
                                             }
                                         } catch (IOException e) {
@@ -113,24 +123,38 @@ public class Main {
         }
     }
 
-    public static void downloadImage(String folder, String url) {
-        File f = new File(folder);
-        if (!f.exists()) {
-            f.mkdirs();
+    public static void saveTwit(RestService restService, Twit twit) {
+        ObjectDTO dto = new ObjectDTO();
+        dto.id = 0;
+        dto.type = "TwitterMessage";
+        dto.fields = new HashMap<>();
+        dto.fields.put("idstr", twit.getIdStr());
+        dto.fields.put("lang", twit.getLang());
+        dto.fields.put("text", twit.getText());
+        dto.fields.put("ts", twit.getTimestampMs());
+        dto.fields.put("userid", twit.getUser().getIdStr());
+        dto.fields.put("username", twit.getUser().getName());
+
+        try {
+            System.out.println("Saving twit: Result: " + restService.createObjects(new ObjectMapper().writeValueAsString(dto)).errorCode);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        for (int i = 0; i <= 2; ++i) {
-            try {
-                URL imageUrl = new URL(url);
-                Path path = Paths.get(folder + url.substring(url.lastIndexOf("/")), new String[0]);
-                if (!path.toFile().exists()) {
-                    Files.copy(imageUrl.openStream(), path, StandardCopyOption.REPLACE_EXISTING);
-                }
-                LoggerFactory.getLogger(Main.class).info("Downloaded: " + url);
-                break;
-            } catch (Exception e) {
-                LoggerFactory.getLogger(Main.class).error("Unable to download: " + url, e);
-                continue;
-            }
+    }
+
+    public static void downloadImage(RestService restService, String folder, String url) {
+        ObjectDTO dto = new ObjectDTO();
+        dto.id = 0;
+        dto.type = "TerraFile";
+        dto.fields = new HashMap<>();
+        dto.fields.put("url", url);
+        dto.fields.put("folder", folder);
+        dto.fields.put("needcheck", Boolean.toString(true));
+
+        try {
+            System.out.println("Save image: Result: " + restService.createObjects(new ObjectMapper().writeValueAsString(dto)).errorCode);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
